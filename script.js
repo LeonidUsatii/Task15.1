3a0e8a55d18ddbc9783e174b2fc2746e58ff7c9b
Leonid Usatii <leonid.usatiy@gmail.com>
2023-07-18 20:42:47 +0200

    init commit

--- script.js
+++ script.js
@@ -8,16 +8,10 @@ function loadToDo() {
   myObject.then((myObject) => {
     dataPromise = myObject.json();
     dataPromise.then((res) => {
-      //console.log(res);
       let arrObject = res;
-
       console.log(arrObject);
-
       const ListUl = document.getElementById('taskUl');
-
       arrObject.forEach((element) => {
-        //console.log('123');
-
         const li = document.createElement('li');
         li.textContent = `userId: ${element.userId} id: ${element.id} 
         title: ${element.title}  completed: ${element.completed}`;
