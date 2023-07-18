1ae66463d04a8bbd1be577b7bf5f6777f36bbbae
Leonid Usatii <leonid.usatiy@gmail.com>
2023-07-18 19:15:46 +0200

    init commit

---
+++ script.js
@@ -0,0 +1,29 @@
+//const ListUl = document.getElementById('taskUl');
+
+let url = 'https://jsonplaceholder.typicode.com/users/1/todos';
+
+function loadToDo() {
+  let myObject = fetch(url);
+  myObject.then((myObject) => {
+    dataPromise = myObject.json();
+    dataPromise.then((res) => {
+      //console.log(res);
+      let arrObject = res;
+
+      console.log(arrObject);
+
+      const ListUl = document.getElementById('taskUl');
+
+      arrObject.forEach((element) => {
+        //console.log('123');
+
+        const li = document.createElement('li');
+        li.textContent = `userId: ${element.userId} id: ${element.id} 
+        title: ${element.title}  completed: ${element.completed}`;
+        ListUl.append(li);
+      });
+    });
+  });
+}
+
+loadToDo();
